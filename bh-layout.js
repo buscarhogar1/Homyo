1fd9d065ff38516d4ca1607bc0e920c5bf64ea15
buscarhogar1 <buscarhogar1@gmail.com>
2026-03-26 22:04:27 +0100

    Add files via upload

--- bh-layout.js
+++ bh-layout.js
@@ -33,20 +33,12 @@ function renderHeader({ showMiniSearch, root }) {
 
         <div class="rightGroup" aria-label="Acciones">
           <a href="#" id="navFavoritos" class="navActionLink">
-            <span class="navActionIcon" aria-hidden="true">
-              <svg viewBox="0 0 24 24" fill="currentColor">
-                <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path>
-              </svg>
-            </span>
+            <span class="navActionIcon" aria-hidden="true">♡</span>
             <span class="navActionText">Favoritos</span>
           </a>
 
           <a href="#" id="navLogin" class="navActionLink">
-            <span class="navActionIcon" aria-hidden="true">
-              <svg viewBox="0 0 24 24" fill="currentColor">
-                <path d="M12 12c2.76 0 5-2.24 5-5S14.76 2 12 2 7 4.24 7 7s2.24 5 5 5zm0 2c-3.33 0-10 1.67-10 5v3h20v-3c0-3.33-6.67-5-10-5z"></path>
-              </svg>
-            </span>
+            <span class="navActionIcon" aria-hidden="true">👤</span>
             <span class="navActionText">Iniciar sesión</span>
           </a>
 
